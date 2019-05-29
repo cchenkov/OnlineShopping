5b95718020178a698753f3d05470e6f62dbc6bc4
Elliiie <eli.georgieva.06@gmail.com>
2019-05-29 18:48:50 +0300

    added comment and rating table and registration form

--- data/init.sql
+++ data/init.sql
@@ -10,4 +10,32 @@ CREATE TABLE User (
     LastName VARCHAR(30) NOT NULL,
     Address VARCHAR(100) NOT NULL,
     PhoneNumber VARCHAR(15) NOT NULL
+
+
+CREATE TABLE Product(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+	Name VARCHAR(20),
+	User_id INT,
+	
+	FOREIGN KEY (User_id) REFERENCES User(Id)
+);
+
+CREATE TABLE Comment (
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    User_id INT,
+    Product_id INT,
+    Message VARCHAR(100),
+    
+    FOREIGN KEY (User_id) REFERENCES User(Id),
+    FOREIGN KEY (Product_Id) REFERENCES Product(Id)
+);
+
+CREATE TABLE Rating(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    User_id INT,
+    Product_id INT,
+    Value INT,
+    
+    FOREIGN KEY (User_id) REFERENCES User(Id),
+    FOREIGN KEY (Product_Id) REFERENCES Product(Id)
 );
