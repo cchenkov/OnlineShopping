8222cf37c2bd9eea0dee316682b6a955ac589db0
Elliiie <eli.georgieva.06@gmail.com>
2019-05-30 17:31:53 +0300

    added ratings and comments input

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-$host = "localhost";
-$username = "tsenko";
-$password = "Helicopter_1500";
-$dbname = "onlinestore";
-$dsn = "mysql:host=$host;dbname=$dbname";
-$options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-);
+	$host = "localhost";
+	$username = "root";
+	$password = "220069";
+	$dbname = "onlinestore";
+	$dsn = "mysql:host=$host;dbname=$dbname";
+	$options = array(
+	    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	);
 
 ?>
--- public_html/details.php
+++ public_html/details.php
@@ -1,5 +1,55 @@
 <?php
+if (isset($_POST['submit'])) {
+  require "../config.php";
+  require "../common.php";
 
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+ 
+    $new_comment = array(
+      "Message" => $_POST['comment'],
+      //"ProductId" => $_POST[$this->product->id]
+    );
+
+    $sql = sprintf(
+      "INSERT INTO %s (%s) values (%s)",
+      "Comment",
+      implode(", ", array_keys($new_comment)),
+      ":" . implode(", :", array_keys($new_comment))
+    );
+
+    $statement = $connection->prepare($sql);
+    $statement->execute($new_comment);
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+}
+
+if (isset($_POST['rate'])) {
+  require "../config.php";
+  require "../common.php";
+
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+ 
+    $new_rating = array(
+      "Value" => $_POST['rating'],
+      //"ProductId" => $_POST[$this->product->id]
+    );
+
+    $sql = sprintf(
+      "INSERT INTO %s (%s) values (%s)",
+      "Rating",
+      implode(", ", array_keys($new_rating)),
+      ":" . implode(", :", array_keys($new_rating))
+    );
+
+    $statement = $connection->prepare($sql);
+    $statement->execute($new_rating);
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+}
 
 ?>
 
@@ -10,12 +60,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/rating_stars.css">
     <title>Document</title>
 
 
 </head>
 <body>
     <h2>Details</h2>
+
+	<?php if (isset($_POST['submit']) && $statement) {
+	  echo escape($_POST['name']); ?> successfully added
+	<?php } ?>
+    <form method="post">
+	<label for="comment">Add Comment</label>
+	<textarea name="comment" rows="5" cols="40"></textarea>
+	<br>
+	<br>
+	<input type="submit" name="submit" value="Submit Comment">
+	<br>
+	<br>
+	<label for="rating">Rating</label>
+  	<input type="number" name="rating" id="rating" min="1" max="5">
+	<br>
+	<br>
+	<input type="submit" name="rate" value="Rate">
+	<br>
+	<br>
+	</form>
+	<button><a href="all_comments.php">Show all comments</a></button> 
 </body>
 
 <?php include "templates/footer.php"; ?>
