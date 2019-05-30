3fc98e29bd80465a40e27d4e6d0e746403d4423a
TsenkoTsenkov <tseni.tsenkov@gmail.com>
2019-05-30 11:15:16 +0300

    Added a read that does work

--- public_html/readProducts.php
+++ public_html/readProducts.php
@@ -1,52 +1,30 @@
 <?php 
    
-	require 'config.php';
-	require 'common.php';
+	require '../config.php';
+	require '../common.php';
     
-    $conn = mysqli_connect($dsn, $username, $password, $options);
+    $conn = mysqli_connect($host, $username, $password, $dbname);
 	
 	if(!$conn){
 		echo 'Connection error: '. mysqli_connect_error();
-    }
-    
-    $sql = 'SELECT ProductName, Price, ImageSource FROM Product ORDER BY created_at';
-	$result = mysqli_query($conn, $sql);
+    }else{
+		$sql = 'SELECT ProductName, Price, ImageSource, Id FROM Product ORDER BY Price';
+		$result = mysqli_query($conn, $sql);
+	}
 	
-	$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-	mysqli_free_result($result);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["Id"]. "<br> Name: " . $row["ProductName"]. "<br> " . $row["ImageSource"]. "<br> Price: " .  $row["Price"]. "<br><br>";
+		}
+		
+	} else {
+		echo "0 results";
+	}
+   
 	mysqli_close($conn);
 
 
 ?>
 
-<!DOCTYPE html>
-<html>
-	
-	<?php include('templates/header.php'); ?>
 
-	<h4 class="center grey-text">Products</h4>
 
-	<div class="container">
-		<div class="row">
-
-			<?php foreach($products as $product){ ?>
-
-				<div class="col s6 md3">
-					<div class="card z-depth-0">
-						<div class="card-content center">
-							<h6><?php echo htmlspecialchars($product['ProdcutName']); ?></h6>
-							<div><?php echo htmlspecialchars($product['Price']); ?></div>
-                            <div><?php echo htmlspecialchars($product['ImageSource']); ?></div>
-						</div>
-						<div class="card-action right-align">
-							<a class="brand-text" href="#">more info</a>
-						</div>
-					</div>
-				</div>
-
-			<?php } ?>
-
-		</div>
-	</div>
-
-</html>
