5c305ca7c91eff19400a99b0b4eb85c9ed1a7e73
TsenkoTsenkov <tseni.tsenkov@gmail.com>
2019-05-30 17:25:25 +0300

    Connecting to database in configFile

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $host = "localhost";
 $username = "tsenko";
 $password = "Helicopter_1500";
@@ -8,5 +10,14 @@ $dsn = "mysql:host=$host;dbname=$dbname";
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 );
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+	
+	if(!$conn){
+		echo 'Connection error: '. mysqli_connect_error();
+    }else{
+		$sql = 'SELECT ProductName, Price, ImageSource, Id FROM Product ORDER BY Price';
+		$result = mysqli_query($conn, $sql);
+	}
 
 ?>
