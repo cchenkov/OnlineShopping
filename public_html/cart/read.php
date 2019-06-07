6e4541ea08a35bfefdd258b5efd54452a799b7be
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2019-06-07 11:46:14 +0300

    Trying to link cart with products and current user, but haven't succeeded still

--- public_html/cart/read.php
+++ public_html/cart/read.php
@@ -1,34 +1,126 @@
 <?php
+	session_start();
+
 	require "../../config.php";
 	require "../../common.php";
 
-	$conn = mysqli_connect($host, $username, $password, $dbname);
+	$success = null;
 
-	if (!$conn) {
-		echo 'Connection error: '. mysqli_connect_error();
-  }
-	else {
-		$sql = 'SELECT UserId, ProductId, Quantity, Id FROM Cart ORDER BY Quantity';
-		$result = mysqli_query($conn, $sql);
-	}
+	if (isset($_POST['order'])) {
+		try {		
+			if(isset($_SESSION['user_id'])) {
+				$connection = new PDO($dsn, $username, $password, $options);
 
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
-            echo "<h3>Quantity:</h3>" . $row["Quantity"] . "<br> ProfileId=" .$row["UserId"] . 
-            '<a href="../user/show.php?id=' . $row["UserId"]  .
-             '"> Profile </a>' . "<br>" . "PoductId=" .
-            $row["ProductId"] .  '<a href="../details.php?Id=' . $row["ProductId"]  . '"> Product </a>' .
-            
-            "<br> 
-            <br>";
+				$product_id = $_POST['order'];
 
-            
+				$sql = "INSERT INTO ProductOrder(UserId, ProductId, Quantity, ApprovalStatus) VALUES(:userId, :productId, :quantity, :approvalStatus)";
+
+				$statement = $connection->prepare($sql);
+
+				$statement->bindValue(':userId', $_SESSION['user_id']);
+				$statement->bindValue(':productId', $product_id);
+				$statement->bindValue(':quantity', 1);
+				$statement->bindValue(':approvalStatus', 'Order sent');
+				$statement->execute();
+
+				echo 'Successfully ordered';
+			}
+			else {
+				die('Please log in to order');
+			}
+
+		} catch(PDOException $error) {
+			echo $sql . "<br>" . $error->getMessage();
+			exit;
 		}
-
-	} else {
-		echo "0 results";
 	}
-	mysqli_close($conn);
+
+	if (isset($_POST["delete"])) {
+		try {
+			$connection = new PDO($dsn, $username, $password, $options);
+
+			$id = $_POST["delete"];
+
+			$sql = "DELETE FROM Cart WHERE Id = :id";
+
+			$statement = $connection->prepare($sql);
+
+			$statement->bindValue(':id', $id);
+			$statement->execute();
+
+			$success = "Product successfully removed from cart";
+		} catch(PDOException $error) {
+			echo $sql . "<br>" . $error->getMessage();
+			exit;
+		}
+	}
+
+	try {
+		if (isset($_SESSION['user_id'])) {
+			$connection = new PDO($dsn, $username, $password, $options);
+
+			$sql = "SELECT c.Id AS Id, p.ProductName AS product, c.Quantity AS quantity, p.Price AS price FROM Cart c
+					INNER JOIN Product p
+					WHERE c.UserId = :userid";
+	
+			$statement = $connection->prepare($sql);
+			$statement->bindValue(':userid', $_SESSION['user_id']);
+			$statement->execute();
+	
+			$result = $statement->fetchAll();
+		} else {
+			die('Please login to see your cart');
+		}
+	
+	} catch (PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+		exit;
+	}
 ?>
 
+<?php include "../templates/header.php"; ?>
+
+<head>
+  <link rel="stylesheet" href="../css/table.css">
+</head>
+
+<?php if ($success) echo escape($success) ?>
+
+<form method="post">
+	<?php
+		if ($result && $statement->rowCount() > 0) { ?>
+			<h1>Cart</h1>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Product</th>
+						<th>Quantity</th>
+						<th>Price</th>
+						<th>Order</th>
+						<th>Remove</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<?php foreach ($result as $row) { ?>
+						<tr>
+							<td><?php echo escape($row["product"]); ?></td>
+							<td><?php echo escape($row["quantity"]); ?></td>
+							<td><?php echo escape($row["price"]); ?></td>
+							<td><button type="submit" name="order" value="<?php echo $row["Id"]; ?>">Order</button></td>
+							<td><button type="submit" name="delete" value="<?php echo $row["Id"]; ?>">Delete</button></td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		<?php } else { ?>
+			<blockquote>No products in cart.</blockquote>
+		<?php }
+	?>
+</form>
+
+<br>
 <a href="../index.php">Back to home</a>
+
+<?php include "../templates/footer.php"; ?>
--- public_html/product/read.php
+++ public_html/product/read.php
@@ -4,54 +4,43 @@
 	
 	session_start();
 
-	$conn = mysqli_connect($host, $username, $password, $dbname);
+	try {
+		$connection = new PDO($dsn, $username, $password, $options);
 
-	if (!$conn) {
-		echo 'Connection error: '. mysqli_connect_error();
-  	}
-	else {
-		$sql = 'SELECT ProductName, Price, ImageSource, Id FROM Product ORDER BY Price';
-		$result = mysqli_query($conn, $sql);
+		$sql = "SELECT Id, ProductName, Price, ImageSource FROM Product ORDER BY Price";
+
+		$statement = $connection->prepare($sql);
+		$statement->execute();
+
+		$result = $statement->fetchAll();
+	}
+	catch (PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+		exit;
 	}
 
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
-			$row_id = $row['Id'];
-
-			echo "<strong>Name: </strong>" . $row["ProductName"] ."<br> ".
-			 $row["ImageSource"]."<br> ".
-			 '<a href="../details.php?Id=' . $row["Id"] . '">More Info</a><br>'.
-			"<br><strong>Price: </strong>" .  $row["Price"] . "<br><br>" . "<form action='read.php' method='post'><button type='submit' name='order' value='$row_id'>Order</button></form>" .
-			"<p>-------------------------</p><br>
-			<br><br>";
-		}
-
-	} else {
-		echo "0 results";
-	}
-	mysqli_close($conn);
-
-	if (isset($_POST['order'])) {
+	if (isset($_POST['add_to_cart'])) {
 		try {		
 			if(isset($_SESSION['user_id'])) {
 				$connection = new PDO($dsn, $username, $password, $options);
 
-				$product_id = $_POST['order'];
+				$product_id = $_POST['add_to_cart'];
 
-				$sql = "INSERT INTO ProductOrder(UserId, ProductId, Quantity, ApprovalStatus) VALUES(:userId, :productId, :quantity, :approvalStatus)";
+				echo '<script>console.log("' . $product_id . '");</script>';
+
+				$sql = "INSERT INTO Cart(UserId, ProductId, Quantity) VALUES(:userId, :productId, :quantity)";
 
 				$statement = $connection->prepare($sql);
 
 				$statement->bindValue(':userId', $_SESSION['user_id']);
 				$statement->bindValue(':productId', $product_id);
 				$statement->bindValue(':quantity', 1);
-				$statement->bindValue(':approvalStatus', 'Order sent');
 				$statement->execute();
 
-				echo 'Successfully ordered';
+				echo 'Successfully added to cart';
 			}
 			else {
-				die('Please log in to order');
+				die('Please log in to add to cart');
 			}
 
 		} catch(PDOException $error) {
@@ -59,8 +48,73 @@
 			exit;
 		}
 	}
+
+	if (isset($_POST["delete"])) {
+		try {
+			$connection = new PDO($dsn, $username, $password, $options);
+
+			$id = $_POST["delete"];
+
+			$sql = "DELETE FROM Product WHERE Id = :id";
+
+			$statement = $connection->prepare($sql);
+
+			$statement->bindValue(':id', $id);
+			$statement->execute();
+
+			$success = "Product successfully deleted";
+		} catch(PDOException $error) {
+			echo $sql . "<br>" . $error->getMessage();
+			exit;
+		}
+	}
 ?>
 
+<?php include "../templates/header.php"; ?>
+
+<head>
+  <link rel="stylesheet" href="../css/table.css">
+</head>
+
+<?php if ($success) echo escape($success) ?>
+
+<form method="post">
+	<?php
+		if ($result && $statement->rowCount() > 0) { ?>
+			<h1>All Products</h1>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Product</th>
+						<th>Price</th>
+						<th>Image</th>
+						<th>More Info</th>
+						<th>Add to cart</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<?php foreach ($result as $row) { ?>
+						<tr>
+							<td><?php echo escape($row["ProductName"]); ?></td>
+							<td><?php echo escape($row["Price"]); ?></td>
+							<td><?php echo escape($row["ImageSource"]); ?></td>
+							<td><a href="../details.php?Id= <?php echo $row["Id"];?> "> Details </a></td>
+							<td><button type="submit" name="add_to_cart" value="<?php echo $row["Id"];?>">Add to cart</button></td>
+							<td><button type="submit" name="delete" value="<?php echo $row["Id"]; ?>">Delete</button></td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		<?php } else { ?>
+			<blockquote>No products found.</blockquote>
+		<?php }
+	?>
+</form>
+
+<br>
 <a href="../index.php">Back to home</a>
 
 <?php include '../templates/footer.php' ?>
